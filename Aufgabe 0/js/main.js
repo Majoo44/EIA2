d7291f6c1052ccd210346098854147b8abbe59b3
Mario Eigeldinger <44026413+Majoo44@users.noreply.github.com>
2019-03-21 20:14:55 +0100

    ja

---
+++ Aufgabe 0/js/main.js
@@ -0,0 +1,8 @@
+/*
+Aufgabe: Aufgabe 0,
+Name: Mario Eigeldinger
+Matrikel: 261167
+Datum: 22.03.2019,
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+*/
+//# sourceMappingURL=main.js.map
---
+++ Aufgabe 0/js/main.ts
@@ -0,0 +1,9 @@
+/*
+Aufgabe: Aufgabe 0, 
+Name: Mario Eigeldinger
+Matrikel: 261167
+Datum: 22.03.2019, 
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+*/
+
+
