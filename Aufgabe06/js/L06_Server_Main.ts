be734f78bd9910a91c6248a553bec2fd45f495ed
Mario Eigeldinger <mario.eigeldinger@hs-furtwangen.de>
2020-05-28 22:50:18 +0200

    ALert Test

--- Aufgabe06/js/L06_Server_Main.ts
+++ Aufgabe06/js/L06_Server_Main.ts
@@ -13,21 +13,32 @@ namespace HaushaltshilfeData {
         let response: Response = await fetch("data.json");
         let offer: string = await response.text();
         let data: Data = JSON.parse(offer);
-        
+
         generateContent(data);
-        let form: HTMLDivElement = <HTMLDivElement> document.querySelector("#form");
+        let form: HTMLDivElement = <HTMLDivElement>document.querySelector("#form");
         form.addEventListener("change", handleChange);
         document.querySelector("#button")?.addEventListener("click", handleClick);
-        
+
 
     }
 
     function handleClick(): void {
-        alert("Danke für deine Bestellung!");
+       // alert("Danke für deine Bestellung!");
+
+        var form = document.querySelector("form");
+        var data = new FormData(<any>form);
+        var req = new XMLHttpRequest();
+        req.send(data);
+
+        let url: string = "https://whatever.server/path/file";
+        let query: URLSearchParams = new URLSearchParams(<any>form);
+        url += url + "?" + query.toString();
+        alert(url);
+        //await fetch(url);
     }
 
     function handleChange(_event: Event): void {
-        let order: HTMLDivElement = <HTMLDivElement> document.querySelector("#order");
+        let order: HTMLDivElement = <HTMLDivElement>document.querySelector("#order");
         order.innerHTML = "";
 
         let data: FormData = new FormData(document.forms[0]);
@@ -35,37 +46,37 @@ namespace HaushaltshilfeData {
 
         for (let entry of data) {
             if (entry[0] == "Produkte") {
-                let item: HTMLInputElement = <HTMLInputElement> document.querySelector("[value='" + entry[1] + "']");
+                let item: HTMLInputElement = <HTMLInputElement>document.querySelector("[value='" + entry[1] + "']");
                 let price: number = Number(item.getAttribute("price"));
-                let amount: number = Number (data.get(entry[1] + "Menge"));
-                let einheit: string = <string> (item.getAttribute("einheit"));
-                
+                let amount: number = Number(data.get(entry[1] + "Menge"));
+                let einheit: string = <string>(item.getAttribute("einheit"));
+
                 total += price * amount;
                 order.innerHTML += item.value + " | " + amount + " " + einheit + ": " + price * amount + " €" + "<br> <br>";
             }
 
             if (entry[0] == "Geld") {
-                let item: HTMLInputElement = <HTMLInputElement> document.querySelector("[value='" + entry[1] + "']");
+                let item: HTMLInputElement = <HTMLInputElement>document.querySelector("[value='" + entry[1] + "']");
                 let betrag: number = Number(data.get("Betrag"));
-                order.innerHTML += item.value + ": " +  betrag + " €" + " <br>" + "<br>";
+                order.innerHTML += item.value + ": " + betrag + " €" + " <br>" + "<br>";
                 total += 5;
             }
 
             if (entry[0] == "Hausarbeiten") {
-                let item: HTMLInputElement = <HTMLInputElement> document.querySelector("[value='" + entry[1] + "']");
-                let price: number = Number(item.getAttribute("price")); 
+                let item: HTMLInputElement = <HTMLInputElement>document.querySelector("[value='" + entry[1] + "']");
+                let price: number = Number(item.getAttribute("price"));
                 total += price;
                 order.innerHTML += item.value + ": " + price + " €" + "<br> <br>";
             }
 
             if (entry[0] == "Zahlung") {
-                let item: HTMLInputElement = <HTMLInputElement> document.querySelector("[value='" + entry[1] + "']");
+                let item: HTMLInputElement = <HTMLInputElement>document.querySelector("[value='" + entry[1] + "']");
                 order.innerHTML += "<hr> Zahlungsmethode: " + item.value + "<br> <br>";
             }
 
         }
-        let supermarkt: string = <string> data.get("Supermarkt");
-        order.innerHTML += "Supermarkt Vorgabe: " + supermarkt + "<br>" + "<hr>" + "<h3>Total(mit Gebühr): " + total + "€<h3><hr>" ;
+        let supermarkt: string = <string>data.get("Supermarkt");
+        order.innerHTML += "Supermarkt Vorgabe: " + supermarkt + "<br>" + "<hr>" + "<h3>Total(mit Gebühr): " + total + "€<h3><hr>";
 
     }
-    }
+}
