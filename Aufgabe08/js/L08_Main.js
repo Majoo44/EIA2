9afff452bdf0f762682eefdc3b606a0f890793fd
Mario Eigeldinger <mario.eigeldinger@hs-furtwangen.de>
2020-06-11 20:23:56 +0200

    Hintergrund Gradient + pattern

--- Aufgabe08/js/L08_Main.js
+++ Aufgabe08/js/L08_Main.js
@@ -8,34 +8,50 @@ Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe...
 */
 let canvas = document.querySelector("canvas");
 let crc2 = canvas.getContext("2d");
-let bakterien = canvas.getContext("2d");
 crc2.canvas.width = window.innerWidth;
 crc2.canvas.height = window.innerHeight;
-crc2.fillStyle = "#830305";
-crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
-crc2.beginPath();
-crc2.arc(20, 20, 20, 0, 2 * Math.PI, true);
-crc2.strokeStyle = "#2958AE";
-crc2.stroke();
-bakterien.fillStyle = "#2958AE";
-bakterien.fillRect(100, 100, 20, 200);
-bakterien.fillRect(500, 100, 20, 200);
-bakterien.beginPath();
-bakterien.ellipse(50, 50, 20, 30, 0, 0, 1, true);
-bakterien.stroke();
-bakterien.strokeStyle = "#2EFF7B";
-bakterien.beginPath();
-bakterien.moveTo(100.1, 10);
-bakterien.lineTo(2.1, 10);
-bakterien.moveTo(4.5, 1);
-bakterien.lineTo(4.5, 10);
-bakterien.moveTo(7.5, 1);
-bakterien.lineTo(10.5, 10);
-bakterien.stroke();
-let gradient = crc2.createLinearGradient(0, 0, 0, 100);
-gradient.addColorStop(0, "black");
-gradient.addColorStop(.5, "red");
-gradient.addColorStop(1, "gold");
+let koerperzellen = canvas.getContext("2d");
+let coronaviren = canvas.getContext("2d");
+let gradient = crc2.createLinearGradient(0, 0, 0, 500);
+gradient.addColorStop(0, "rgb(180, 0, 76)");
+gradient.addColorStop(1, "rgb(250, 0, 23)");
 crc2.fillStyle = gradient;
-crc2.fillRect(0, 0, 200, 100);
+crc2.fillRect(0, 0, canvas.width, canvas.height);
+/*function drawBackground(): void {
+let gradient: CanvasGradient = crc2.createLinearGradient (0, 0, 0, crc2.canvas.height);
+gradient.addColorStop(0, "lightblue");
+gradient.addColorStop(0, "blue");
+
+crc2.fillStyle = gradient;
+crc2.fillRect(0, 0, canvas.width, canvas.height);
+
+}
+*/
+//crc2.fillStyle = "#830305";
+//crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
+let pattern = document.createElement("canvas").getContext("2d");
+pattern.canvas.width = 40;
+pattern.canvas.height = 20;
+pattern.fillStyle = "Transparent";
+pattern.fillRect(0, 0, pattern.canvas.width, pattern.canvas.height);
+pattern.moveTo(0, 10);
+pattern.lineTo(10, 10);
+pattern.lineTo(20, 0);
+pattern.lineTo(30, 0);
+pattern.lineTo(40, 10);
+pattern.lineTo(30, 20);
+pattern.lineTo(20, 20);
+pattern.lineTo(10, 10);
+pattern.stroke();
+crc2.fillStyle = crc2.createPattern(pattern.canvas, "repeat");
+crc2.fillRect(0, 0, canvas.width, canvas.height);
+koerperzellen.beginPath();
+koerperzellen.arc(300, 300, 150, 0, 2 * Math.PI, true);
+koerperzellen.fillStyle = ("#2958AE");
+koerperzellen.strokeStyle = ("rgb(200, 200, 200)");
+koerperzellen.arc(600, 600, 25, 0, 2 * Math.PI, true);
+koerperzellen.fillStyle = ("#2958AE");
+koerperzellen.strokeStyle = ("rgb(200, 200, 200)");
+koerperzellen.fill();
+koerperzellen.stroke();
 //# sourceMappingURL=L08_Main.js.map
--- Aufgabe08/js/L08_Main.ts
+++ Aufgabe08/js/L08_Main.ts
@@ -7,43 +7,61 @@ Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe...
 */
 let canvas: HTMLCanvasElement = <HTMLCanvasElement>document.querySelector("canvas");
 let crc2: CanvasRenderingContext2D = <CanvasRenderingContext2D>canvas.getContext("2d");
-let bakterien: CanvasRenderingContext2D = <CanvasRenderingContext2D>canvas.getContext("2d");
 crc2.canvas.width = window.innerWidth;
 crc2.canvas.height = window.innerHeight;
 
-crc2.fillStyle = "#830305";
-crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
+let koerperzellen: CanvasRenderingContext2D = <CanvasRenderingContext2D>canvas.getContext("2d");
+let coronaviren: CanvasRenderingContext2D = <CanvasRenderingContext2D>canvas.getContext("2d");
 
-crc2.beginPath();
-crc2.arc(20, 20, 20, 0, 2 * Math.PI, true);
-crc2.strokeStyle = "#2958AE";
+let gradient: CanvasGradient = crc2.createLinearGradient(0, 0, 0, 500);
 
-crc2.stroke();
-
-bakterien.fillStyle = "#2958AE";
-bakterien.fillRect(100, 100, 20, 200);
-bakterien.fillRect(500, 100, 20, 200);
-
-bakterien.beginPath();
-bakterien.ellipse(50, 50, 20, 30, 0, 0, 1, true); 
-bakterien.stroke();
-bakterien.strokeStyle = "#2EFF7B";
-
-
-bakterien.beginPath();
-bakterien.moveTo(100.1, 10);
-bakterien.lineTo(2.1, 10);
-bakterien.moveTo(4.5, 1);
-bakterien.lineTo(4.5, 10);
-bakterien.moveTo(7.5, 1);
-bakterien.lineTo(10.5, 10);
-bakterien.stroke();
-
-let gradient: CanvasGradient = crc2.createLinearGradient(0, 0, 0, 100);
-
-gradient.addColorStop(0, "black");
-gradient.addColorStop(.5, "red");
-gradient.addColorStop(1, "gold");
+gradient.addColorStop(0, "rgb(180, 0, 76)");
+gradient.addColorStop(1, "rgb(250, 0, 23)");
 
 crc2.fillStyle = gradient;
-crc2.fillRect(0, 0, 200, 100);
+crc2.fillRect(0, 0, canvas.width, canvas.height);
+
+
+
+/*function drawBackground(): void {
+let gradient: CanvasGradient = crc2.createLinearGradient (0, 0, 0, crc2.canvas.height);
+gradient.addColorStop(0, "lightblue");
+gradient.addColorStop(0, "blue");
+
+crc2.fillStyle = gradient;
+crc2.fillRect(0, 0, canvas.width, canvas.height);
+
+}
+*/
+//crc2.fillStyle = "#830305";
+//crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
+
+let pattern: CanvasRenderingContext = document.createElement("canvas").getContext("2d");
+pattern.canvas.width = 40;
+pattern.canvas.height = 20;
+
+pattern.fillStyle = "Transparent";
+pattern.fillRect(0, 0, pattern.canvas.width, pattern.canvas.height);
+pattern.moveTo(0, 10);
+pattern.lineTo(10, 10);
+pattern.lineTo(20, 0);
+pattern.lineTo(30, 0);
+pattern.lineTo(40, 10);
+pattern.lineTo(30, 20);
+pattern.lineTo(20, 20);
+pattern.lineTo(10, 10);
+pattern.stroke();
+
+crc2.fillStyle = crc2.createPattern(pattern.canvas, "repeat");
+crc2.fillRect(0, 0, canvas.width, canvas.height);
+
+koerperzellen.beginPath();
+koerperzellen.arc(300, 300, 150, 0, 2 * Math.PI, true);
+koerperzellen.fillStyle = ("#2958AE");
+koerperzellen.strokeStyle = ("rgb(200, 200, 200)");
+koerperzellen.arc(600, 600, 25, 0, 2 * Math.PI, true);
+koerperzellen.fillStyle = ("#2958AE");
+koerperzellen.strokeStyle = ("rgb(200, 200, 200)");
+koerperzellen.fill();
+koerperzellen.stroke();
+
